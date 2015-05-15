fd218e4d8eeb6b454e27ed1e037b58fcebf685bb
strengur <steingrimur@740.is>
2015-05-15 16:48:05 +0000

    Wordpress hooked up
    Finished adding wordpress basic system and database settings.

---
+++ wp-config.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', '24_740is');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/** Set the URL for the site */
+define('WP_HOME', 'http://localhost/740.is');
+define('WP_SITEURL', 'http://localhost/740.is');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ':jN=GJDE, p&YsV.9(BF|B;V;8A$=owWf]TO7;xO$AsI8Gmqp[R:-mtJKoW<T`KC');
+define('SECURE_AUTH_KEY',  'NU^xD:F-3@@K(xC:pKGmzrgm I[!Mq{/-,/0cbjq[s5&g/$z.*P-|>}`~FdAqt:q');
+define('LOGGED_IN_KEY',    'AxIp/Vv Oc<1X4K0l-c O1D|9W1R#b@ ^8t=h;s1Hl}W/}T*5c*Uf(UNMlWA_kU>');
+define('NONCE_KEY',        'Gx^,Gu65x?7+3nD2Q0P QF$yqGZG11MUh,|{>F+pI5i]m+JR$$CQ9{[.904RZ=8h');
+define('AUTH_SALT',        'qsW{6mIh;:iyC<~v%`#@q.-#%%xHd4:k$9e%fcC:U3Vh/p9k{h%9)e7|[,iCdWlF');
+define('SECURE_AUTH_SALT', 'yd#~(hr|eRigoIdXmbv3z<`spDK3-!Wos)]JVNFOQkFs5[PsLn<=DNP<*?Lqo-W*');
+define('LOGGED_IN_SALT',   'i{.PC^j-gksUf&fE?/-;B kw(D;TL1Z9yQL_#l?Dd3JY`.)y-V|:<SUV<x+U~7 &');
+define('NONCE_SALT',       '>VC^RR5Y!z88B+d@wE0S/(CjgJ?p?UX@OSyiiXG[Z0fs}c5H3&8>ZxJU`Y_wSVj>');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_740_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/740-template/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+
+<html>
+	<head><meta charset="UTF-8" />
+		<title>740.is - Vefsíður</title>
+		<link rel="stylesheet" type="text/css" href="normalize.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:400,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:300,500,300italic,500italic' rel='stylesheet' type='text/css'>
+		
+<!-- 		<link rel="stylesheet" href="http://basehold.it/30/ff00ff"> -->
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body>
+		<nav>
+			<div class="logo">
+				<img src="images/740is-logo-163px.png" alt="Merki 740.is">
+			</div>
+			
+			<div class="menu-items">
+				<ul>
+						<li><a href="#heim" class="button-white">Heim</a></li>
+						<li><a href="#verkefni" class="button-white">Verkefni</a></li>
+						<li><a href="#umokkur" class="button-white">Um 740</a></li>
+						<li id="display-contact-form"><a href="#" class="button-white">Hafa samband</a></li>
+				</ul>
+			</div>
+			
+			<div class="mobile">
+				<img src="images/icons/menu.svg" class="" alt="Snjalltækja hnappur">
+			</div>
+				
+		</nav>
+	
+		<!--
+			IMAGE BACKGROUND WITH BLURRED SECTION FOR TEXT
+		-->
+		
+		<section class="landing" id="heim">
+			
+			<div class="landing-welcome">
+				<h1>Þú ert á réttum stað!</h1>
+				<h4>Láttu okkur um netsíðuna þína</h4>
+			</div>
+			
+			<div class="landing-contact">
+				<?php include('contact.php'); ?>
+			</div>
+			
+		</section>
+		
+		<section class="intro" id="kynning">
+			<div class="content three-column" id="">  <!-- ID MISSING -->
+				
+				<div class="intro-content-row-one">
+					<center><img src="images/740-intro-img-new.png" alt="Hringlaga útlínumynd af vefsíðu"></center>
+					<h4>Vefsíðugerð</h4>
+					<span class="none-mobile-hr"><hr></span>
+					<p>Við leggjum metnað okkar í að hanna flottar, stílhreinar vefsíður sem aðlaga sig að snjallsímum og öðrum snjalltækjum. Passað er upp á að síðurnar séu léttar til að auðveldara sé að skoða þær í símum. Vertu með aðlaðandi útlit á vefsíðunni þinni því það skiptir máli!</p>
+<!-- 					<center><button>Smelltu hér</button></center> -->
+					<span class="mobile-hr"><hr></span>
+				</div>
+				
+				
+				<div class="intro-content-row-two">
+					<center><img src="images/740-intro-img-online.png" alt="Hringlaga mynd af innkaupakerru"></center>
+					<h4>Netverslun</h4>
+					<span class="none-mobile-hr"><hr></span>
+					<p>Notendavæn og traust netverslun sem við sérsníðum að þínu útliti. Sérlega einföld í notkun og hægt að hafa ýmsa greiðsluvalmöguleika. Vertu með vörurnar þínar til sölu allan sólarhringinn!</p>
+<!-- 					<center><button>Smelltu hér</button></center> -->
+					<span class="mobile-hr"><hr></span>
+				</div>
+				
+				<div class="intro-content-row-three">
+					<center><img src="images/740-intro-img-nytt-utlit.png" alt="Orðin NÝTT ÚTLIT inn í hring"></center>
+					<h4>Útlitsbreyting</h4>
+					<span class="none-mobile-hr"><hr></span>
+					<p>Er langtsíðan að vefsíðan hjá þér var uppfærð síðast eða er netsíðan hjá þér lítt spennandi, ónotendavæn og beinlínis fráhrindandi. Það er kominn tími til að gefa núverandi netsíðu nútímalegt og stílhreint útlit - Hafðu samband!</p>
+<!-- 					<center><button>Smelltu hér</button></center> -->
+<!-- 					<span id="mobile-hr"><hr></span> -->
+				</div>
+			</div>
+		</section>
+		
+		
+		<!--
+			IMAGE BACKGROUND WHERE landing and intro SECTION's SCROLL OVER
+		-->
+		
+		<section class="image-gap image-2" id="verkefni">
+			<div class="container">
+				<h1>FYRRI VERKEFNI</h1>
+				<center><a href="#" class="button-white">Skoða Öll</a></center>
+  			</div>
+		</section>
+		
+		<section class="testimonials">
+			<h2>SÍÐUSTU VERKEFNI</h2>
+			<div class="testimonials-left">
+				<center><img src="images/ll-netverslun.png" alt="Mynd af tilbúinni vefsíðu" width="250px"></center>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis a nibh id gravida. Vestibulum sed sodales enim, eget pretium tortor. Fusce accumsan accumsan dignissim. Vivamus sollicitudin tristique felis, sit amet iaculis dui vulputate a. Curabitur placerat neque sed mi venenatis tincidunt. Vestibulum eget tempor nulla, ut vehicula velit. Quisque lobortis tortor interdum lectus suscipit, id pharetra augue facilisis.</p>
+				<center><a href="#" class="button-gray">Skoða Nánar</a></center>
+			</div>
+			
+			<div class="testimonials-right">
+				<center><img src="images/thinnlikami-netsida.jpg" alt="Mynd af tilbúinni vefsíðu" width="250px"></center>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis a nibh id gravida. Vestibulum sed sodales enim, eget pretium tortor. Fusce accumsan accumsan dignissim. Vivamus sollicitudin tristique felis, sit amet iaculis dui vulputate a. Curabitur placerat neque sed mi venenatis tincidunt. Vestibulum eget tempor nulla, ut vehicula velit. Quisque lobortis tortor interdum lectus suscipit, id pharetra augue facilisis.</p>
+				<center><a href="#" class="button-gray">Skoða Nánar</a></center>
+			</div>
+			
+		</section>
+		
+		<section class="image-gap image-1" id="umokkur">
+			<div class="container">
+				<h1>UM OKKUR</h1>
+  			</div>
+		</section>
+		
+		<section class="content">
+			<div class="content-text">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis a nibh id gravida. Vestibulum sed sodales enim, eget pretium tortor. Fusce accumsan accumsan dignissim. Vivamus sollicitudin tristique felis, sit amet iaculis dui vulputate a. Curabitur placerat neque sed mi venenatis tincidunt. Vestibulum eget tempor nulla, ut vehicula velit. Quisque lobortis tortor interdum lectus suscipit, id pharetra augue facilisis. Aenean cursus, augue at consequat efficitur, ante ligula consectetur quam, vel eleifend tortor quam sed odio. Sed congue hendrerit justo eget condimentum. Nulla nisi nibh, porta vel cursus in, iaculis eget turpis. Phasellus a eros diam. Vestibulum nec neque tristique purus ornare feugiat sed vitae felis. Nullam quis viverra libero. In in turpis sit amet nunc egestas pretium.</p>
+			</div>
+		</section>
+		
+		<footer>
+			<h4>&copy;2014-<?php echo date("Y"); ?> | 740.is | Allur&nbsp;réttur áskilinn.</h4>
+		</footer>
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/main-java.js"></script>
+	</body>
+</html>
