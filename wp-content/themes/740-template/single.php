bfcbf052527049e5dc7e9fae149f21590b3d169d
strengur <steingrimur@740.is>
2015-06-02 17:09:53 +0000

    Singla page coding started

--- wp-content/themes/740-template/single.php
+++ wp-content/themes/740-template/single.php
@@ -1,1 +1,12 @@
 <?php get_header(); ?>
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<h2><?php the_title_attribute(); ?></h2>
+
+<?php the_content(); ?>
+
+<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
