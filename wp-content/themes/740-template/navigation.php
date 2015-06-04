b8043d20fc0f7d83f9d5783ceea541ab319bc41c
strengur <steingrimur@740.is>
2015-06-04 22:18:21 +0000

    Color of menu items changed

--- wp-content/themes/740-template/navigation.php
+++ wp-content/themes/740-template/navigation.php
@@ -3,9 +3,9 @@
 		<a href="<?php home_url(); ?>"><img src="<?php bloginfo('template_directory');?>/images/740is-logo1.svg" alt="Merki 740.is"></a>
 	</div>
 
-	<div class="menu-items button-white">
+	<div class="menu-items button-green">
 		<ul>
-			<li><a href="<?php home_url(); ?>">Heim</a></li>
+			<li><a href="http://740.is">Heim</a></li>
 			<li><a href="#verkefni">Verkefni</a></li>
 			<li><a href="#umokkur">Um 740</a></li>
 			<li id="display-contact-form"><a href="#">Hafa samband</a></li>
--- wp-content/themes/740-template/single.php
+++ wp-content/themes/740-template/single.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
 
+<section class="single-post">
+	<div class="single-post-content">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		
+		<h2><?php the_title_attribute(); ?></h2>
+		
+		<?php the_content(); ?>
+		
+		<?php endwhile; else : ?>
+			<p><?php _e( 'Því miður er ekkert til að birta hér.' ); ?></p>
+		<?php endif; ?>
+	</div>
+</section>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-<h2><?php the_title_attribute(); ?></h2>
-
-<?php the_content(); ?>
-
-<?php endwhile; else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+<?php get_footer(); ?>
