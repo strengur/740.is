1f8f2bde48bf50bc98a0c40416b26ac2d8820543
strengur <steingrimur@740.is>
2015-06-10 08:41:42 +0000

    Posts listing page, continue

--- wp-content/themes/740-template/verkefni.php
+++ wp-content/themes/740-template/verkefni.php
@@ -14,6 +14,18 @@ foreach ( $postslist as $post ) :
 		<div class="list-of-posts-background list-of-posts-content">
 			<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1><i style="font-size: 14px;"><?php the_date(); ?> </i>
 			<?php the_excerpt(); ?>
+			
+			<?php
+				$firstPhoto = array( 'post_type' => 'attachment', 'posts_per_page' => -1, 'post_status' =>'any', 'post_parent' => $post->ID ); 
+				$attachments = get_posts( $firstPhoto );
+				if ( $attachments ) {
+					foreach ( $attachments as $attachment ) {
+						the_attachment_link( $attachment->ID , false );
+					}
+				} else {
+					echo 'Mynd vantar';
+				}
+			?>
 			<div class="button-gray"><a href="<?php the_permalink(); ?>">Skoða Nánar</a></div>
 		</div>
   	
